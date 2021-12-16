2b8558fe7ff831b5ad66bda467d8e12854d8c120
chaourarimine <imine.chaourar07@gmail.com>
2021-12-16 13:50:23 +0100

    informations filtrées selon la date

--- api/src/DataFixtures/AppFixtures.php
+++ api/src/DataFixtures/AppFixtures.php
@@ -10,20 +10,71 @@ use EasyRdf\Literal\Date;
 
 class AppFixtures extends Fixture
 {
+
+
     public function load(ObjectManager $manager): void
     {
 
-        if (($open = fopen(dirname(__FILE__).'/data/valeursfoncieres-2021-s1.csv', "r")) !== FALSE) {
-            $a=0;
-            while (($data = fgetcsv($open, 1000, "|")) !== FALSE && $a<10000) {
-                $array[] = $data;
-                $vente = new Ventes();
-                $vente->setDate(new DateTime())
-                ->setPrixMoyenM2(123)
-                ->setNombreVentes(123)
-                ->setRegion($data[9]);
+        //set php memory limit
+        ini_set('memory_limit', '18024M');
 
-                $manager->persist($vente);
+        //create an empty list of Ventes
+        $listVente = array();
+
+        if (($open = fopen(dirname(__FILE__) . '/data/valeursfoncieres-2021-s1.csv', "r")) !== FALSE) {
+            $a = 0;
+            while (($data = fgetcsv($open, 1000, "|")) !== FALSE && $a < 50000) {
+                //skip the first line
+                if ($a > 1) {
+                    $array[] = $data;
+
+                    //if type = aprtement or maison
+                    $type = $data[36];
+                    if (strcmp($type, "Maison") == 0 || strcmp($type, "Appartement") == 0) {
+
+                        //if it's a "Vente"
+                        $typeVente = $data[9];
+                        $valeurFociere = (int) $data[10];
+                        $surface = (int) $data[42];
+                        $date = $data[8];
+                       
+                        if (strcmp($typeVente, "Vente") == 0 && $valeurFociere > 0 && $surface > 0) {
+
+                            //if this key doesn't exist
+                            if (!array_key_exists($date, $listVente)) {
+
+
+                                //set date to the english format 
+
+                                $str_arr = explode("/", $date);
+                                $date = $str_arr[1] . "/" . $str_arr[0] . "/" . $str_arr[2];
+
+
+                                $prix_moy_m² =  $valeurFociere /  $surface;
+
+
+                                $vente = new Ventes();
+                                $vente->setDate(new DateTime($date))
+                                    ->setPrixMoyenM2($prix_moy_m²)
+                                    ->setNombreVentes(1)
+                                    ->setRegion(gettype($data[42]));
+
+
+                                $listVente[$date] = $vente;
+
+                            } else { //if key exists
+                                $vente = $listVente[$date];
+                                $prix_moy_m² = $vente->getPrixMoyenM2();
+                                $vente->setNombreVentes($vente->getNombreVentes() + 1);
+
+                                $prix_moy_m² = ($prix_moy_m² + ($valeurFociere / $surface)) ;// $vente->getNombreVentes();
+                                
+
+                                $listVente[$date] = $vente;
+                            }
+                        }
+                    }
+                }
                 $a++;
             }
 
@@ -31,6 +82,12 @@ class AppFixtures extends Fixture
         }
         // $product = new Product();
         // $manager->persist($product);
+
+        foreach ($listVente as &$vente) {
+            $vente->setPrixMoyenM2($vente->getPrixMoyenM2() / $vente->getNombreVentes());
+            $manager->persist($vente);
+        }
+
 
         $manager->flush();
     }
