458133cd5b72048aed3ba57bc97e24b400d44a27
idirlb <idir.lacheb23@gmail.com>
2022-01-02 03:48:52 +0100

    barchart et un peu desing

--- pwa/components/ventes/bar_chart.js
+++ pwa/components/ventes/bar_chart.js
@@ -10,10 +10,72 @@ import React from "react";
 
 
 
-const drawChart =  (data,test) => {
- console.log("dataaaa........;"+test)
+const drawChart = (data,test) => {
+  const width = 900;
+  const height = 450;
+  const margin = { top: 50, bottom: 50, left: 50, right: 50 };
 
+  d3.select("#the_SVG_ID2").remove();
+  //let data = this.props.data
 
+  const svgContainer = d3.select("#my_datavizBars").node();
+
+  const svg = d3.select("#my_datavizBars")
+  .append('svg')
+  .attr("id", "the_SVG_ID2")
+  .attr('width', width - margin.left - margin.right)
+  .attr('height', height - margin.top - margin.bottom)
+  .attr("viewBox", [0, 0, width, height]);
+
+  const x = d3.scaleBand()
+  .domain(d3.range(data.length))
+  .range([margin.left, width - margin.right])
+  .padding(0.1)
+
+  const y = d3.scaleLinear()
+  .domain([0, d3.max(data, function (d) { return d.nombre_ventes; })])
+  .range([height - margin.bottom, margin.top])
+
+  svg
+  .append("g")
+  .attr("fill", 'royalblue')
+  .selectAll("rect")
+  //.data(data.sort((a, b) => d3.descending(a.nombre_ventes, b.nombre_ventes)))
+  .data(data)
+  .join("rect")
+      .attr("x", (d, i) => x(i))
+      .attr("y", d => y(d.nombre_ventes))
+      .attr('title', (d) => d.nombre_ventes)
+      .attr("class", "rect")
+      .attr("height", d => y(0) - y(d.nombre_ventes))
+      .attr("width", x.bandwidth());
+
+  svg.append("g")
+      .attr("transform", `translate(${margin.left}, 0)`)
+      .call(d3.axisLeft(y).ticks(null, data.format))
+      .attr("font-size", '20px')
+  
+  if(test === "month"){
+      svg.append("g")
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x).tickFormat(i => data[i].year + '/' + data[i].month))
+      .attr("font-size", '20px')
+  }
+  if(test === "year"){
+      svg.append("g")
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x).tickFormat(i => data[i].year))
+      .attr("font-size", '20px')
+  }
+  
+  if(test === "day"){
+      svg.append("g")
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x).tickFormat(i => data[i].date.substring(0,9)))
+      .attr("font-size", '20px')
+  }
+
+  svg.node();
 }
 
 
@@ -37,15 +99,14 @@ export const BarChart = ({ test , bars , datatest }) => {
 
 
   React.useEffect(async () => {
-    drawChart(data,test);
-  }, [data]);
+    drawChart(bars,test);
+  }, [bars]);
 
 
   return (
     <div id="chartBars">
       <div id="my_datavizBars">
-        type : {test}
-
+        
       </div>
     </div>
 
--- pwa/pages/ventes/serieT.js
+++ pwa/pages/ventes/serieT.js
@@ -8,7 +8,6 @@ import  DonutChart  from "../../components/ventes/pie_chart";
 import { BarChart } from "../../components/ventes/bar_chart";
 import React, { useState, useEffect } from 'react';
 
-
 const donutData = [
   {name: "region 1", value: 19},
   {name: "region 2", value: 20},
@@ -54,30 +53,62 @@ function Page ( { collection , collectionSerie, collectionbars }
       <div>
         <Head>
         <script src="https://d3js.org/d3.v7.min.js"></script>
-
+        <link rel="stylesheet" href="https://bootswatch.com/5/lux/bootstrap.min.css"/>
           <title>test</title>
         </Head>
       </div>
 
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
-      <Serie ventes ={ collectionSerie["hydra:member"] } />
-      <DonutChart data={collection["hydra:member"]}  />
+          <div class="collapse navbar-collapse" id="navbarColor01">
+            <ul class="navbar-nav me-auto">
+              <li class="nav-item">
+                <a class="nav-link active" href="#serie">Série Temporelle
+                  <span class="visually-hidden">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#piechart">Diagramme Circulaire</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#barchart">Diagramme à Barre</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
+      <div className="container" style={{'margin-top': '20px'}} > 
+        <Serie ventes ={ collectionSerie["hydra:member"] }/>
+        <DonutChart data={collection["hydra:member"]}  />
+        
 
-      <div>
-        <select onChange={e => handleChangeYear(e.target.value)} style={{ 'margin-left': '20%' }} >
-          <option value="day">Day</option>
-          <option value="month" selected="true" >Month</option>
-          <option value="year" >Year</option>
-        </select>
-        <input onChange={e => handleChangeDebut(e.target.value)} type="date" id="start" name="trip-start"/>
-        <input type="date" onChange={e => handleChangeFin(e.target.value)} id="end" name="trip-start"/>
+        <div>
+          <select className="form-select bg-dark" onChange={e => handleChangeYear(e.target.value)}  >
+            <option value="day">Day</option>
+            <option value="month" selected="true" >Month</option>
+            <option value="year" >Year</option>
+          </select>
+          <input className="form-control bg-secondary" onChange={e => handleChangeDebut(e.target.value)} type="date" id="start" name="trip-start"/>
+          <input className="form-control bg-secondary" type="date" onChange={e => handleChangeFin(e.target.value)} id="end" name="trip-start"/>
+        </div>
+        <div class="row" style={{'margin-top': '20px'}}>
+          <div className="col-6" style={{'margin-top': '120px'}}>
+              <h4> type date : {type}</h4>
+              <h4> date debut : {debut}</h4>
+              <h4> date fin : {fin}</h4>
+          </div>
+          <div className= "col-6"><BarChart bars={data} test={type}/></div>
+          
+          
+        </div>
       </div>
-      <BarChart bars ={ data }  test ={ type }/>
-      <p> type date : {type}</p>
-      <p> date debut : {debut}</p>
-      <p> date fin : {fin}</p>
-
+      
     </div>
   );
 }
